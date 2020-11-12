cd9f7b638b01e7604cbd44030f3ef20b433a413f
Samuel Goodwin <samuel@roundwallsoftware.com>
2020-11-12 20:14:57 +0100

    Initial Commit

---
+++ CocoaPodsUsing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CocoaPodsUsing
+//
+//  Created by Samuel Goodwin on 11/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
